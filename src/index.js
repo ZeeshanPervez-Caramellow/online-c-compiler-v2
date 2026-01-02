fdbec62a5a7356414d5adf38649570577f749cf8
ZeeshanPervez-Caramellow <zeeshanpervez8@gmail.com>
2026-01-02 17:18:18 +0530

    included body-parser and introduced base routes for app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import supabase from './supabasecliet.js';
+import dotenv from 'dotenv';
+import app from './app.js';
+dotenv.config({
+    path: './.env'
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+}); 
